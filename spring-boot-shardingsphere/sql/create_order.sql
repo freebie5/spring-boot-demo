eaf3bd809a877c8f1427658a9f9c23c5eafe31ea
john <1007954564@qq.com>
2020-12-22 14:55:06 +0800

    float double demo

--- spring-boot-shardingsphere/sql/create_order.sql
+++ spring-boot-shardingsphere/sql/create_order.sql
@@ -3,5 +3,6 @@ CREATE TABLE `t_order_0` (
     `name` varchar(255) NOT NULL,
     `id` bigint(32) NOT NULL,
     `user_id` bigint(32) NOT NULL,
+    `money` decimal(20,2) NOT NULL,
     PRIMARY KEY (`id`) USING BTREE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
